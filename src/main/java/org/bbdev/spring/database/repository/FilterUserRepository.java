ffffad63ded144503277ce30d6a4ace0eb3ed2c8
bokobb <bilalovbakhodir@gmail.com>
2025-10-30 20:27:29 +0500

    JDBS Starter

--- src/main/java/org/bbdev/spring/database/repository/FilterUserRepository.java
+++ src/main/java/org/bbdev/spring/database/repository/FilterUserRepository.java
@@ -1,10 +1,17 @@
 package org.bbdev.spring.database.repository;
 
+import org.bbdev.spring.database.entity.Role;
 import org.bbdev.spring.database.entity.User;
+import org.bbdev.spring.dto.PersonalInfo;
 import org.bbdev.spring.dto.UserFilter;
 
 import java.util.List;
 
 public interface FilterUserRepository {
     List<User> findAllByFilter(UserFilter filter);
+
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
+
+    void updateCompanyAndRole(List<User> users);
+    void updateCompanyAndRoleNamed(List<User> users);
 }
--- src/test/java/org/bbdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/org/bbdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -26,6 +26,19 @@ class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
+    void checkBatch(){
+        var users = userRepository.findAll();
+        userRepository.updateCompanyAndRole(users);
+        System.out.println();
+    }
+
+    @Test
+    void checkJdbcTemplate() {
+        var users = userRepository.findAllByCompanyIdAndRole(1, Role.USER);
+        assertThat(users).hasSize(1);
+    }
+
+    @Test
     @Commit
     void checkAuditing() {
         var ivan = userRepository.findById(1L).get();
